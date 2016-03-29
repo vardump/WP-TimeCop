aa0e89595fa27ef8b32bb62301ae2b6fa4776e72
Prappo Prince <prappo.prince@gmail.com>
2016-03-30 02:23:45 +0600

    few changes

--- lib/required_files.php
+++ lib/required_files.php
@@ -11,4 +11,8 @@ function my_admin_menu() {
 	#submenus
 	add_submenu_page("timecophome", "Add new project", "Add new Project", 0, "addnewproject", "addNewProjectPage");
 	add_submenu_page("timecophome", "Add new Worker", "Add new Worker", 1, "addnewworker", "addNewWorkerPage");
+	add_submenu_page("timecophome", "Add new Team", "Add new Team", 2, "addnewteam", "addNewTeamPage");
+	add_submenu_page("timecophome", "Assign Project", "Assign project", 3, "assignproject", "assignProjectPage");
+	add_submenu_page("timecophome", "Work diary", "Work diary", 4, "workdiary", "workDiaryPage");
+	add_submenu_page("timecophome", "Reports", "Reports", 5, "timecopreports", "reportsPage");
 }
--- pages/home.php
+++ pages/home.php
@@ -1,2 +1,6 @@
 <br>
+<div align="center">
+
 <img height="150" width="150" src="<?php echo logo();?>">
+<h1>Welcome to TimeCop</h1>
+</div>
