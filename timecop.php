2dcad52b9a8c8476527700d91d360b7647aeff33
Prappo Prince <prappo.prince@gmail.com>
2016-03-30 00:37:02 +0600

    added first page

---
+++ timecop.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: TimeCop
+ * Plugin URI: https://github.com/prappo/timecopWp
+ * Description: This is wordpress version of timecop
+ * Version: 1.0
+ * Author: Prappo Islam Prince
+ * Author URI: http://github.com/prappo
+ * License:GPL2
+ */
