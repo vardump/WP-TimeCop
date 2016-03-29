a922faddf7c7d01f9ed876c9bb065b4dbdeeb969
Prappo Prince <prappo.prince@gmail.com>
2016-03-30 02:04:11 +0600

    few changes

--- lib/required_files.php
+++ lib/required_files.php
@@ -5,5 +5,10 @@ require 'functions.php';
 add_action( 'admin_menu', 'my_admin_menu' );
 
 function my_admin_menu() {
+	#main menu
 	add_menu_page( 'TimeCop', 'TimeCop', 'manage_options', 'timecophome', 'homePage', icon(), 0  );
+
+	#submenus
+	add_submenu_page("timecophome", "Add new project", "Add new Project", 0, "addnewproject", "addNewProjectPage");
+	add_submenu_page("timecophome", "Add new Worker", "Add new Worker", 1, "addnewworker", "addNewWorkerPage");
 }
